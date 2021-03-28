19696938123b1fce4b8677994111195b4fb24721
khai96_ <hvksmr1996@gmail.com>
2021-03-28 20:29:37 +0700

    refactor: Re-apply sane-fmt

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import path from 'path'
-import fs from 'fs'
-import { spawnSync } from 'child_process'
-import { platform, exit } from 'process'
 import { group, setFailed, getInput, addPath, setOutput } from '@actions/core'
+import { spawnSync } from 'child_process'
+import fs from 'fs'
 import { safeLoad } from 'js-yaml'
 import fetch from 'node-fetch'
+import path from 'path'
+import { platform, exit } from 'process'
 import tempPath from 'unique-temp-path'
 import { upstreamVersion } from './upstream-version.json'
 
 const location = tempPath('sane-fmt.')
 const executableBaseName = platform === 'win32' ? 'sane-fmt.exe' : 'sane-fmt'
 const executablePath = path.join(location, executableBaseName)
-const upstreamUrlPrefix =
-  'https://github.com/sane-fmt/sane-fmt/releases/download'
+const upstreamUrlPrefix = 'https://github.com/sane-fmt/sane-fmt/releases/download'
 const upstreamBaseNames: Partial<Record<NodeJS.Platform, string>> = {
   darwin: 'sane-fmt-x86_64-apple-darwin',
   linux: 'sane-fmt-x86_64-unknown-linux-gnu',
